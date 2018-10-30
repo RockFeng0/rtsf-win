bedb7a0eaac31528b4b872c7de5d629e4f6a3f9b
rockfeng0 <lkf20031988@163.com>
2018-10-30 19:49:38 +0800

    update version to v1.1

--- winuidriver/__about__.py
+++ winuidriver/__about__.py
@@ -20,7 +20,7 @@ Provide a function for the automation test
 
 __title__           = "rtsf-win"
 __short_desc__      = "only for windows UI test, base on rtsf. e.g. WPF UI or MFC UI"
-__version__         = 1.0
+__version__         = 1.1
 __autor__           = u"罗科峰"
 __author_email__    = "lkf20031988@163.com"
 __license__         = 'MIT'
