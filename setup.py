cb2971eb75b1dcef2119969dae0c7ab7a5885a81
RockFeng0 <lkf20031988@163.com>
2018-11-04 10:27:40 +0800

    update version to v1.2.1, support data-driven

--- setup.py
+++ setup.py
@@ -20,7 +20,55 @@ UI and Web Http automation frame for python.
 '''
 
 from winuidriver import __about__
-from setuptools import setup, find_packages
+import os, io, sys
+from shutil import rmtree
+from setuptools import setup, find_packages, Command
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open("README.md", encoding='utf-8') as f:
+    long_description = f.read()
+
+class UploadCommand(Command):
+    """ Build and publish this package.
+        Support setup.py upload. Copied from requests_html.
+    """
+
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in green color."""
+        print("\033[0;32m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        """ override
+        """
+        pass
+
+    def finalize_options(self):
+        """ override
+        """
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Publishing git tags…')
+        os.system('git tag v{0}'.format(__about__.__version__))
+        os.system('git push --tags')
+
+        sys.exit()
 
 install_requires = [
     "uiautomation",
@@ -31,6 +79,8 @@ setup(
         name = __about__.__title__,
         version=__about__.__version__,        
         description=__about__.__short_desc__,
+        long_description=long_description,
+        long_description_content_type='text/markdown',
         author=__about__.__autor__,
         author_email=__about__.__author_email__,
         url=__about__.HOME_PAGE,
@@ -41,8 +91,12 @@ setup(
         install_requires=install_requires,
         extras_require={},
         entry_points={
-        'console_scripts': [
-            'windriver=winuidriver.cli:main_run'
-        ]
-    },
+            'console_scripts': [
+                'windriver=winuidriver.cli:main_run'
+            ]
+        },
+        # $ setup.py upload support.
+        cmdclass={
+            'upload': UploadCommand
+        }
     )
